cbf47a1d209c2cf9eeef4ee91b6a9bd7870ea3d0
lpreston618 <lpreston618@gmail.com>
2022-07-08 23:14:10 -0500

    Liam's changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ WIDTH, HEIGHT = 1000, 500
 
 def main():
     pygame.init()
+    #switch_list = []
 
     screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
 
@@ -14,7 +15,7 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             
-            if event.type == pygame.mouse.get_pressed():
+            #if event.type == pygame.mouse.get_pressed():
                 
     
     pygame.quit()
@@ -24,10 +25,22 @@ main()
 
 class SWITCH:
     def __init__(self,x,y):
-        self.x = x
-        self.y = y
-        self.dim = ()
-        self.pic_on = open('assets/switch-on.jpg')
-        self.pic_off = open('assests/switch-off.jpg')
+        self.loc = (x,y)
+        self.dim = (100,100)
+        self.pic_on = pygame.image.load('assets/switch-on.jpg')
+        self.pic_off = pygame.image.load('assets/switch-off.jpg')
+        self.current_pic = self.pic_off
         self.state = False
-        self.rect = pygame.rect((x,y),(w,h))
+        self.rect = pygame.rect(self.loc,self.dim)
+    
+    def changeState(self):
+        if self.state == False:
+            self.state = True
+            self.current_pic = self.pic_on
+
+        if self.state == True:
+            self.state = False
+            self.current_pic = self.pic_off
+
+    def initializeOnScreen(self):
+        screen.blit(self.current_pic)
